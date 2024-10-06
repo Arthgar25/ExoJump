1348dba5add01437562463039963725941b1c6e0
PUNPUNCODER <nestor101ayala@gmail.com>
2024-10-06 08:32:53 -0700

    Nomas para hace pmerge

--- ExoJumpBuild/js/script.js
+++ ExoJumpBuild/js/script.js
@@ -1,4 +1,4 @@
-import * as THREE from '//unpkg.com/three/build/three.module.js';
+import * as THREE from '//unpkg.com/three/build/three.module.js'; //gola
 
 
 window.addEventListener('resize', () => {
